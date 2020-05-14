f5ed1195b23be75ebaf922695625c55fd2080b72
João Victor <joaodjtr@gmail.com>
2020-05-14 03:37:53 -0300

    Drawing cards

--- modules/game.js
+++ modules/game.js
@@ -1,9 +1,32 @@
+"use strict"
+
 import {fadeIn, fadeOut} from './helper.js'
 
+let board
+let cards = []
+
 export function drawGame(numberCards, target){
+
+    board = document.createElement('DIV')
+    board.classList.add('board', `board-${numberCards}`)
+    target.appendChild(board)
+
+    for(let i = 0; i < numberCards; i++){
+        let card = document.createElement('DIV')
+        card.classList.add('card')
+        board.appendChild(card)
+        cards.push(card)
+    }
+
     fadeIn(target)
 }
 
 export function endGame(target){
     fadeOut(target)
+
+    cards.forEach(card => board.removeChild(card))
+    target.removeChild(board)
+
+    cards = []
+    board = null
 }
