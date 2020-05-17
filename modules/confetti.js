5421472e95f164078ef62be11ed52df41397e0d9
João Victor <joaodjtr@gmail.com>
2020-05-17 01:13:06 -0300

    Fixing confetti speed

--- modules/confetti.js
+++ modules/confetti.js
@@ -1,11 +1,13 @@
 "use strict"
 
 const sectionHome = document.getElementById('home')
-let canvas, ctx, confsNum, confs, w, h
+let canvas, ctx, confsNum, confs, w, h, req
+let counter = 0
 
 function initializeCanvas(){
     canvas = document.createElement('canvas')
     canvas.classList.add('confetti')
+    canvas.classList.add(counter)
     sectionHome.appendChild(canvas)
     ctx = canvas.getContext('2d')
     w = canvas.width = window.innerWidth
@@ -22,10 +24,12 @@ export const initConfetti = () =>{
 } 
 
 export const clearConfetti = () => {
-    if(canvas) sectionHome.removeChild(canvas)
+  if(req) cancelAnimationFrame(req)
+  if(canvas) sectionHome.removeChild(canvas)
 }
+
 function loop() {
-  requestAnimationFrame(loop);
+  req = requestAnimationFrame(loop);
 	ctx.clearRect(0,0,w,h);
   
   confs.forEach((conf) => {
@@ -35,10 +39,11 @@ function loop() {
 }
 
 function Confetti () {
-    const colours = ['#ff9aa2','#ffb7b2','#ffdac1','#e2f0cb','#b5ead7','#c7ceea'];
+    const colours = ['#cc99c9','#9ec1cf','#9ee09e','#fdfd97','#feb144','#ff6663'];
 
     this.x = Math.round(Math.random() * w);
     this.y = Math.round(Math.random() * h)-(h/2);
+    
     this.rotation = Math.random()*360;
 
     const size = Math.random()*(w/60);
@@ -49,26 +54,24 @@ function Confetti () {
     this.speed = this.size/3;
     
     const opacity = Math.random();
-    this.opacity = opacity < 50 ? 75 : opacity
+    this.opacity = opacity < .5 ? .75 : opacity
 
     this.shiftDirection = Math.random() > 0.5 ? 1 : -1;
 }
 
 Confetti.prototype.border = function() {
-  if (this.y >= h) {
-    this.y = h;
-  }
+  this.y = h;
 }
 
 Confetti.prototype.update = function() {
-  this.y += this.speed;
-  
-  if (this.y <= h) {
+    this.y += this.speed;
+
+    if (this.y <= h) {
     this.x += this.shiftDirection/3;
     this.rotation += this.shiftDirection*this.speed/100;
-  }
+    }
 
-  if (this.y > h) this.border();
+    if (this.y > h) this.border();
 };
 
 Confetti.prototype.draw = function() {
